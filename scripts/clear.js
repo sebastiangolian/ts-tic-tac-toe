4d8ee7b7ac6b25f4c0a4ad059882f40b768287b3
Sebastian Golian <sebastiangolian@gmail.com>
2019-01-25 21:54:13 +0100

    update deploy scripts

--- scripts/clear.js
+++ scripts/clear.js
@@ -1,5 +1,7 @@
 const fs = require('fs-extra');
 
-// remove dist directory
+console.log('Remove dist directory')
+fs.removeSync('dist');
+
 console.log('Remove public/js directory')
 fs.removeSync('public/js');
